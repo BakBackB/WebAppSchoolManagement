1ac916ce15618705f5b7565b575c4bde341f0578
BakBackB <phucmy20061511@gmail.com>
2026-05-24 08:16:03 +0700

    Update ClassScheudle with getter/setter

--- backend/src/main/java/com/school_management/model/ClassSchedule.java
+++ backend/src/main/java/com/school_management/model/ClassSchedule.java
@@ -14,4 +14,64 @@ public class ClassSchedule {
     private SchoolClass schoolClass;
     private Subject subject;
     private Room room;
+    public int getScheduleId() {
+        return scheduleId;
+    }
+    public void setScheduleId(int scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+    public int getClassId() {
+        return classId;
+    }
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+    public int getSubjectId() {
+        return subjectId;
+    }
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+    public int getRoomId() {
+        return roomId;
+    }
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+    public Time getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+    public Time getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+    public SchoolClass getSchoolClass() {
+        return schoolClass;
+    }
+    public void setSchoolClass(SchoolClass schoolClass) {
+        this.schoolClass = schoolClass;
+    }
+    public Subject getSubject() {
+        return subject;
+    }
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+    public Room getRoom() {
+        return room;
+    }
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 }
