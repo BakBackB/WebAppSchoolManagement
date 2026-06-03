fce7b800470297ed3356ec50b54214b4ff365965
thinhisme6 <thinhquangpham2906@gmail.com>
2026-06-04 01:26:10 +0700

    Updated schedule management and subject management features, including new controllers, DAOs, services and some schema data.

--- backend/src/main/java/com/school_management/model/TeachingAssignment.java
+++ backend/src/main/java/com/school_management/model/TeachingAssignment.java
@@ -2,8 +2,14 @@ package com.school_management.model;
 
 public class TeachingAssignment {
     private int assignmentId;
+    private int classId;
+    private int subjectId;
     private int teacherId;
     private int scheduleId;
+     private SchoolClass schoolClass;
+    private Subject subject;
+    private Teacher teacher;
+
 
     public TeachingAssignment() {}
 
@@ -11,14 +17,75 @@ public class TeachingAssignment {
         this.teacherId = teacherId;
         this.scheduleId = scheduleId;
     }
+     public TeachingAssignment(int classId, int subjectId, int teacherId) {
+        this.classId = classId;
+        this.subjectId = subjectId;
+        this.teacherId = teacherId;
+    }
+
+    public int getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(int assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
 
     // Getters and Setters
-    public int getAssignmentId() { return assignmentId; }
-    public void setAssignmentId(int assignmentId) { this.assignmentId = assignmentId; }
 
-    public int getTeacherId() { return teacherId; }
-    public void setTeacherId(int teacherId) { this.teacherId = teacherId; }
+    public int getScheduleId() {
+        return scheduleId;
+    }
 
-    public int getScheduleId() { return scheduleId; }
-    public void setScheduleId(int scheduleId) { this.scheduleId = scheduleId; }
+    public SchoolClass getSchoolClass() {
+        return schoolClass;
+    }
+
+    public void setSchoolClass(SchoolClass schoolClass) {
+        this.schoolClass = schoolClass;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public void setScheduleId(int scheduleId) {
+        this.scheduleId = scheduleId;
+    }
 }
